945d219370dd154ef8e980ff9000331513055b61
Cristiano Morgante <cristianomorgante@hotmail.com>
2024-11-08 00:48:43 -0300

    update dht plugin

--- dht.plugin.zsh
+++ dht.plugin.zsh
@@ -1,33 +1,23 @@
-# dht.plugin.zsh
+# Carregar funções
+fpath+=("${0:A:h}/functions")
+autoload -Uz dht
 
-# Determina o diretório base do plugin
-DHT_PLUGIN_DIR="${0:A:h}"
-
-# Carrega o arquivo de configuração (prioriza o config do usuário)
+# Carregar configurações
 if [[ -f "$HOME/.dht_config" ]]; then
   source "$HOME/.dht_config"
 else
-  source "$DHT_PLUGIN_DIR/config/dht.conf"
+  source "${0:A:h}/config/dht.conf"
 fi
 
-# Carrega a função principal do Dev Helper Tools (dht)
-source "$DHT_PLUGIN_DIR/bin/dht"
-
-# Carrega as funções de verificação de dependências
-source "$DHT_PLUGIN_DIR/functions/dht.zsh"
-
-# Carrega aliases com base nas configurações
+# Carregar aliases selecionados pelo usuário
 if [[ "$DHT_LOAD_GIT_ALIASES" == "true" ]]; then
-  source "$DHT_PLUGIN_DIR/aliases/git_aliases.zsh"
-  echo "Git aliases enabled."
+  source "${0:A:h}/aliases/git_aliases.zsh"
 fi
 
 if [[ "$DHT_LOAD_LARAVEL_ALIASES" == "true" ]]; then
-  source "$DHT_PLUGIN_DIR/aliases/laravel_aliases.zsh"
-  echo "Laravel aliases enabled."
+  source "${0:A:h}/aliases/laravel_aliases.zsh"
 fi
 
 if [[ "$DHT_LOAD_SYSTEM_ALIASES" == "true" ]]; then
-  source "$DHT_PLUGIN_DIR/aliases/system_aliases.zsh"
-  echo "System aliases enabled."
+  source "${0:A:h}/aliases/system_aliases.zsh"
 fi
